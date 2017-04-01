bf4cf20f2a59a19d2ec93e07e97396817c4a011e
QOO <p61402@gmail.com>
2017-04-01 21:19:54 +0800

    Add files via upload

---
+++ 100-1999/496.py
@@ -0,0 +1,26 @@
+"""
+#		    Problem	            Language	Run Time	Submission Date
+19090932	496	Simply Subsets	PYTH3	    0.020	    2017-04-01 13:14:25
+"""
+while True:
+    try:
+        list_A = input().split()
+    except EOFError:
+        break
+
+    list_B = input().split()
+
+    set_A = set(list_A)
+    set_B = set(list_B)
+
+    lengthOfA_join_B = len(set_A & set_B)
+    if lengthOfA_join_B == len(set_A) and lengthOfA_join_B == len(set_B):
+        print("A equals B")
+    elif lengthOfA_join_B == len(set_A):
+        print("A is a proper subset of B")
+    elif lengthOfA_join_B == len(set_B):
+        print("B is a proper subset of A")
+    elif lengthOfA_join_B == 0:
+        print("A and B are disjoint")
+    else:
+        print("I'm confused!")
