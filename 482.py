651725d6989730923e8fc398144b051908987b8e
p61402 <p61402@gmail.com>
2017-04-01 16:13:48 +0800

    Update 482.py
    Use OrderedDict to improve the efficiency.

--- 482.py
+++ 482.py
@@ -1,15 +1,23 @@
+"""
+Ranking	Submission	Run Time	Language	Submission Date
+3471	19089566	0.040	    PYTH3	    2017-04-01 08:06:45
+"""
+from collections import OrderedDict
+
 numOfcases = int(input())
 
 for n in range(numOfcases):
     input()
     index_array = input().split()
     value_array = input().split()
-    #print(index_array)
-    #print(value_array)
-    for i in range(1, len(index_array) + 1):
-        for index in index_array:
-            if int(index) == i:
-                print(value_array[index_array.index(index)])
-                break
+
+    index_dict = {}
+    for i in range(len(index_array)):
+        index_dict[i] = int(index_array[i])
+    sort_index_dict = OrderedDict(sorted(index_dict.items(), key=lambda t: t[1]))
+
+    for k in sort_index_dict.keys():
+        print(value_array[k])
+
     if n != numOfcases - 1:
         print()
